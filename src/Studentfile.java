03ac8736f73509456d67cd97efe16a8c91775d2b
chuchumj <janeibodje@gmail.com>
2018-11-29 22:22:38 -0500

    Initial commit

---
+++ src/Studentfile.java
@@ -0,0 +1,60 @@
+// class for student records
+class Studentfile{
+
+    private String firstname ;
+    private String surname;
+    private int age ;
+    private char sex;
+    private double gpa;
+    Studentfile file;
+
+
+
+    public Studentfile(String firstname,String surname,int age,char sex,double gpa){
+
+        this.firstname = firstname;
+        this.surname = surname;
+        this.age = age;
+        this.sex = sex;
+        this.gpa = gpa;
+    }
+
+    public Studentfile(String firstname, String surname){
+        this.firstname = firstname;
+        this.surname = surname;
+    }
+
+    public Studentfile(){
+
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+   public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public String toString() {
+        return  " User details \n "+"Firstname:" +getFirstname()+", Lastname:"+ getSurname()+", Age:"+getAge()+", Sex:"+getSex()+" GPA:"+getGpa();
+    }
+
+    public static void main(String[] args){
+        Studentfile file = new StudentfileBuilder().setFirstname("Ade").setSurname("Kemi").setAge(2).setGpa(2.3).setSex('F').build();
+        System.out.println(file);
+    }
+}
---
+++ src/StudentfileBuilder.java
@@ -0,0 +1,44 @@
+// student records (builder pattern)
+public class StudentfileBuilder {
+    private String firstname;
+    private String surname;
+    private int age;
+    private char sex;
+    private double gpa;
+
+    public StudentfileBuilder setFirstname(String firstname){
+        this.firstname = firstname;
+        return this;
+    }
+
+    public StudentfileBuilder setSurname(String surname){
+        this.surname = surname;
+        return this;
+    }
+
+    public StudentfileBuilder setAge(int age){
+        this.age = age;
+        return this;
+    }
+
+    public StudentfileBuilder setSex(char sex){
+        this.sex = sex;
+        return this;
+    }
+
+    public StudentfileBuilder setGpa(double gpa){
+        this.gpa = gpa;
+        return this;
+    }
+
+    public Studentfile build() {
+        return new Studentfile(firstname,surname,age,sex,gpa);
+    }
+
+    public StudentfileBuilder reset()
+
+    {
+        return null;
+    }
+
+}
